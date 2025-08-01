ebbc9de8346b6d896ce8266f9d4a1f584aee7b5d
алексей федулов <fab-78@mail.ru>
2025-08-02 00:57:58 +0200

    corrected the product.py and test_product.py

--- src/product.py
+++ src/product.py
@@ -1,13 +1,40 @@
-
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price if self.validate_price(price) else 0.0
         self.quantity = quantity
 
+    @property
+    def price(self) -> float:
+        """Геттер для приватного атрибута price"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер для приватного атрибута __price с проверкой на положительность"""
+        if self.validate_price(new_price):
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+    @staticmethod
+    def validate_price(price: float) -> bool:
+        """Проверяем, что цена положительная"""
+        return price > 0
+
+    @classmethod
+    def new_product(cls, product_data: dict) -> "Product":
+        """Создаёт и возвращает новый экземпляр класса Product из словаря."""
+        return cls(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"],
+        )
--- tests/test_product.py
+++ tests/test_product.py
@@ -11,3 +11,12 @@ def test_product_initialization():
     assert product.description == "4K OLED"
     assert product.price == 99999.99
     assert product.quantity == 3
+
+
+def test_price_validation():
+    """Проверка только валидации цены"""
+    p = Product("Тест", "Тест", 1000.0, 1)
+    p.price = 1500.0
+    assert p.price == 1500.0
+    p.price = -500
+    assert p.price == 1500.0
