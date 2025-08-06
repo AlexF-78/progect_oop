90e0bcd75a5356356bed2aca3fa76156df52fca0
алексей федулов <fab-78@mail.ru>
2025-08-06 23:25:26 +0200

    corrected product.py and test_product.py

--- src/product.py
+++ src/product.py
@@ -10,6 +10,14 @@ class Product:
         self.__price = price if self.validate_price(price) else 0.0
         self.quantity = quantity
 
+    def __str__(self) -> str:
+        """ Строковое представление продукта в формате: Название, цена руб. Остаток: количество шт."""
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other: "Product") -> float:
+        """Сложение продуктов (цена * количество)"""
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
     @property
     def price(self) -> float:
         """Геттер для приватного атрибута price"""
@@ -38,3 +46,18 @@ class Product:
             price=product_data["price"],
             quantity=product_data["quantity"],
         )
+
+
+def total_quantity(*products: Product) -> float:
+    """Функция для подсчёта общей стоимости продуктов"""
+    total = 0.0
+    for emp in products:
+        total += emp.price * emp.quantity
+    return round(total, 2)
+
+# p1 = Product("Ноутбук", "", 100000, 5) # 500000
+# p2 = Product("Телефон", "", 80000, 10) # 800000
+# p3 = Product("Наушники", "", 5000, 20) # 100000
+#
+# result = total_quantity(p1, p2, p3)
+# print(result)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,4 @@
-# import pytest
-
-from src.product import Product
+from src.product import Product, total_quantity
 
 
 def test_product_initialization():
@@ -20,3 +18,38 @@ def test_price_validation():
     assert p.price == 1500.0
     p.price = -500
     assert p.price == 1500.0
+
+
+def test_product_str():
+    """Тест строкового представления продукта"""
+    product = Product("Смартфон", "Android 128GB", 59999.99, 10)
+
+    # Проверяем формат вывода
+    expected_str = "Смартфон, 59999.99 руб. Остаток: 10 шт."
+    assert str(product) == expected_str
+
+    # Проверяем с обновлёнными значениями
+    product.price = 54999.99
+    product.quantity = 5
+    expected_str_updated = "Смартфон, 54999.99 руб. Остаток: 5 шт."
+    assert str(product) == expected_str_updated
+
+
+def test_product_addition():
+    p1 = Product("Ноутбук", "", 100000, 2)  # 200000
+    p2 = Product("Телефон", "", 80000, 3)  # 240000
+    assert p1 + p2 == 440000  # 200000 + 240000
+
+
+def test_total_quantity(sample_products):
+    """Тест функции total_quantity"""
+    p1, p2, p3 = sample_products
+
+    # Проверяем сумму всех трёх продуктов
+    assert total_quantity(p1, p2, p3) == 1289999.7
+
+    # Проверяем сумму двух продуктов
+    assert total_quantity(p1, p2) == 1049999.85
+
+    # Проверяем с одним продуктом
+    assert total_quantity(p1) == 599999.9
